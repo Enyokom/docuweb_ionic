0366c7d5c336ea8a61224cbb55841b228d77f2d0
unknown <k.tekou@net.estia.fr>
2022-03-28 11:17:21 +0200

    origine master

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
@@ -7,6 +7,36 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  EtapesApiUrl = '';
+  EtapesData = {
+    Nom: '',
+    Description: '',
+    Statut: ''
+  };
 
+  searchTitle = '';
+
+  constructor(private http: HttpClient) {
+    this.readAPI('http://localhost:3000/etapes/')
+    .subscribe((data) => {
+    console.log(data);
+  });
+  }
+
+  readAPI(URL: string) {
+    return this.http.get(URL);
+  }
+
+  getEtapes() {
+    const search = encodeURIComponent(this.searchTitle).trim();
+    this.EtapesApiUrl = 'http://localhost:3000/etapes/' + search;
+    this.readAPI(this.EtapesApiUrl)
+      .subscribe((data) => {
+      console.log(data);
+      this.EtapesData.Nom = data['Nom'];
+      this.EtapesData.Description = data['Description'];
+      this.EtapesData.Statut = data['Statut'];
+
+    });
+}
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  title: 'Default Environment',
+  apiURL: 'http://localhost:3000',
+  authEndPoint: 'http://localhost:3000'
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
